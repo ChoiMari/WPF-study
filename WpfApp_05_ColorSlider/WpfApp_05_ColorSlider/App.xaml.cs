ecec286a334e5865be0e5367d2d61ff50b9acf6d
ChoiMari <chl98639863@gmail.com>
2025-05-23 02:06:12 +0900

    실습. 컬러슬라이드

---
+++ WpfApp_05_ColorSlider/WpfApp_05_ColorSlider/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp_05_ColorSlider
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
